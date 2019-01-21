aeaed6e7a7d9ff7e2f7abff21a637647b667169c
Rafay <abdulrafay@khi.iba.edu.pk>
2019-01-21 23:34:00 +0500

    forms complete

--- src/app/assessment/assessment.component.ts
+++ src/app/assessment/assessment.component.ts
@@ -7,24 +7,61 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AssessmentComponent implements OnInit {
 	selectedOption=0;
-	sum;
-	choices: any =[
-		0,
-		1,
-		2,
-		3,
+  sum;
+  answered: any =[ 
+    ['question1',0],
+    ['question2',0],
+    ['question3',0],
+    ['question4',0],
+    ['question5',0],
+    ['question6',0],
+    ['question7',0],
+    ['question8',0],
+    ['question9',0],
+    ['question10',0],
+    ['question11',0],
 
-	];
+  ];  
+  points:any=[]
+  choices: any =[
+    0,
+    1,
+    2,
+    3,
+  ];
   	
+   point:number; 
+ 
+
   	radioChangeHandler (event:any){
   		
   	
+       let answered = this.answered;
+      
+         for (let i=0; i < answered.length;i++){
+           // console.log('i:',answered[i][0],event.target.name)
+          if(event.target.name==answered[i][0]){
+            // console.log('match')
+            answered[i][1] = parseInt(event.target.value)
+          }
+           // console.log(answered[i][1])      
+         }
+         this.getSum(answered)
+      
+      
+      
 
-  		this.selectedOption = parseInt(event.target.value) + this.selectedOption;
-  		
-
-  		console.log('sum',this.selectedOption);
+      
   	 }
+     
+     getSum(answered){
+       let sum = 0;
+       console.log('in func',answered)
+       for(let i =0 ; i < answered.length; i++){
+         sum += parseInt(answered[i][1])
+       }
+       this.selectedOption=sum;
+     }
   	constructor() { }
 
   	ngOnInit() {
--- src/app/fiqr/fiqr.component.ts
+++ src/app/fiqr/fiqr.component.ts
@@ -12,6 +12,31 @@ export class FiqrComponent implements OnInit {
   domain3sum=0;
   finalScore=0;
 	sum;
+
+  answered: any =[ 
+    ['domain1-1',0],
+    ['domain1-2',0],
+    ['domain1-3',0],
+    ['domain1-4',0],
+    ['domain1-5',0],
+    ['domain1-6',0],
+    ['domain1-7',0],
+    ['domain1-8',0],
+    ['domain1-9',0],
+    ['domain2-1',0],
+    ['domain2-2',0],
+    ['domain3-1',0],
+    ['domain3-2',0],
+    ['domain3-3',0],
+    ['domain3-4',0],
+    ['domain3-5',0],
+    ['domain3-6',0],
+    ['domain3-7',0],
+    ['domain3-8',0],
+    ['domain3-9',0],
+    ['domain3-10',0],
+  ];
+
 	choices: any =[
 		
 		1,
@@ -29,24 +54,43 @@ export class FiqrComponent implements OnInit {
 	];
 	
 	radioChangeHandler1 (event:any){
-  		this.domain1sum = parseInt(event.target.value) + this.domain1sum;
-  		let temp = this.domain1sum + this.domain2sum + this.domain3sum;
-  		this.finalScore = this.domain1sum/3 + this.domain3sum/2;
-  	}
 
-  	radioChangeHandler2 (event:any){
-  		this.domain2sum = parseInt(event.target.value) + this.domain2sum;
-   		let temp = this.domain1sum + this.domain2sum + this.domain3sum;
-  		this.finalScore = this.domain1sum/3 + this.domain3sum/2;
+      let answered=this.answered;
+      for (let i=0; i < answered.length;i++){
+          if(event.target.name==answered[i][0]){
+            answered[i][1] = parseInt(event.target.value)
+          }
+           console.log(answered[i][1])      
+      }
+      this.getSum(answered)
   	}
-
-	radioChangeHandler3 (event:any){
-		this.domain3sum = parseInt(event.target.value) + this.domain3sum;
-  		let temp = this.domain1sum + this.domain2sum + this.domain3sum;
-  		this.finalScore = this.domain1sum/3 + this.domain3sum/2;
-	}
   	
-  
+  getSum(answered){
+       let sum1=0;
+       let sum2=0;
+       let sum3=0;
+
+       // console.log('in func',answered)
+       for(let i =0 ; i < 9 ; i++){
+         sum1 += parseInt(answered[i][1])
+       }
+       this.domain1sum = sum1;
+
+       for(let i =9 ; i < 11 ; i++){
+         sum2 += parseInt(answered[i][1])
+       }
+       this.domain1sum = sum1;
+
+       for(let i =11 ; i < answered.length ; i++){
+         sum3 += parseInt(answered[i][1])
+       }
+       this.domain1sum = sum1;
+       this.domain2sum = sum2;
+       this.domain3sum = sum3;
+       // console.log('sum1,sum2,sum3', sum1,sum2,sum3);
+
+       this.finalScore = sum1/3 + sum2 + sum3/2;
+     }
 
   	constructor() { }
 
